6e87bb6401b19955ac804dd93a9969c5cf7ae17c
shassan95 <syed.hassan.cobc@gmail.com>
2017-11-15 13:50:09 +0000

    Added last database record classes.

---
+++ src/java/com/model/Claim.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Owner
+ */
+public class Claim {
+    
+    private int id;
+    
+    private String memId;
+    
+    private Date date;
+    
+    private String rationale;
+    
+    private String status;
+    
+    private float amount;
+
+    public Claim(int id, String memId, Date date, String rationale, String status, float amount) {
+        this.id = id;
+        this.memId = memId;
+        this.date = date;
+        this.rationale = rationale;
+        this.status = status;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMemid() {
+        return memId;
+    }
+
+    public void setMemid(String memid) {
+        this.memId = memid;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getRationale() {
+        return rationale;
+    }
+
+    public void setRationale(String rationale) {
+        this.rationale = rationale;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    
+    
+    
+}
---
+++ src/java/com/model/Payment.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ *
+ * @author Owner
+ */
+public class Payment {
+    private int id;
+    
+    private int memId;
+    
+    private String typeOfPayment;
+    
+    private float amount;
+    
+    private Time time;
+    
+    private Date date;
+
+    public Payment(int id, int memid, String typeOfPayment, float amount, Time time, Date date) {
+        this.id = id;
+        this.memId = memid;
+        this.typeOfPayment = typeOfPayment;
+        this.amount = amount;
+        this.time = time;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMemid() {
+        return memId;
+    }
+
+    public void setMemid(int memid) {
+        this.memId = memid;
+    }
+
+    public String getTypeOfPayment() {
+        return typeOfPayment;
+    }
+
+    public void setTypeOfPayment(String typeOfPayment) {
+        this.typeOfPayment = typeOfPayment;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+}
